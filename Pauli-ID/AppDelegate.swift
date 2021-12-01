c8a9556bcaa4d046d42658ccfff5c2e141206c3e
blankgus <blankgus@gmail.com>
2021-12-01 13:36:47 -0300

    Add files via upload

---
+++ Pauli-ID/AppDelegate.swift
@@ -0,0 +1,118 @@
+//
+// AppDelegate.swift
+//
+// Copyright 2021 (c) WebIntoApp.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of 
+// this software and associated documentation files (the "Software"), to deal in the 
+// Software without restriction, including without limitation the rights to use, 
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+// Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+//
+// Pauli-ID
+//
+// Created by Colégio Paulicéia on 24/11/2021.
+//
+import UIKit
+import UserNotifications
+import Firebase
+import AppTrackingTransparency
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    let gcmMessageIDKey = "gcm.message_id"
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                        FirebaseApp.configure()
+                        Messaging.messaging().delegate = self
+                        if #available(iOS 10.0, *) {
+                            UNUserNotificationCenter.current().delegate = self
+                            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+                            UNUserNotificationCenter.current().requestAuthorization(
+                                options: authOptions,
+                                completionHandler: {_, _ in })
+                        } else {
+                            let settings: UIUserNotificationSettings =
+                                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+                            application.registerUserNotificationSettings(settings)
+                        }
+                        application.registerForRemoteNotifications()
+        UINavigationBar.appearance().customNavigationBar()
+        return true
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+    }
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        completionHandler([])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        completionHandler()
+    }
+}
+extension AppDelegate : MessagingDelegate {
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        Messaging.messaging().subscribe(toTopic: "iOS-Users")
+    }
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        let dataDict:[String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        Messaging.messaging().subscribe(toTopic: "iOS-Users")
+    }
+}
